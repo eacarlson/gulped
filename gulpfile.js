203257dea480838a51c1df1fc23c63a3e8b2b473
eacarlson <erika.a.carlson@gmail.com>
2015-01-20 20:34:41 -0500

    Adding uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function() {
     gulp.src('./src/js/*.js')
       .pipe(concat('main.js'))
-      .pipe(gulp.dest('./public/js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/js'));
 })
